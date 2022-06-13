3071cdd43f4b5e149f98c859f974d16c99a4585a
ElierHP <elierhp93@gmail.com>
2022-06-13 12:44:50 -0400

    Add: User Show, Update, Delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: %i[ show update destroy ]
+
+    # GET /users/1
+    def show
+        render json: @user
+    end
+
+    # POST /users
     def create
         @user = User.new(user_params)
     
@@ -9,8 +17,27 @@ class UsersController < ApplicationController
         end
       end
 
+      
+    # PATCH/PUT /users/1
+    def update
+        if @user.update(user_params)
+        render json: @user
+        else
+        render json: @user.errors, status: :unprocessable_entity
+        end
+    end
+
+    # DELETE /users/1
+    def destroy
+        @user.destroy
+    end
+
       private 
       def user_params
         params.require(:user).permit(:email, :password, :password_confirmation)
+      end
+
+      def set_user
+        @user = User.find(params[:id])
       end
 end
